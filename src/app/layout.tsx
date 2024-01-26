3f19b4982614d46f180d537346df52c90f8e5dbd
inspirezuza <inspire7220@gmail.com>
2024-01-27 02:06:20 +0700

    Update metadata in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "ProbSolve-score",
-  description: "because im lazy to count score myself",
+  title: "My-ProbSolve-score",
+  description: "because im lazy too count score myself",
 };
 
 export default function RootLayout({
